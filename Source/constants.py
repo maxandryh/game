23f9dc4df441146a0ca0722a7d3e798be12ce31b
deadinside666ua <129310872+AndrianaOz@users.noreply.github.com>
2023-05-27 18:22:06 +0300

    l
    gg

--- Source/constants.py
+++ Source/constants.py
@@ -7,5 +7,7 @@ SCREEN_SIZE = (1280, 720)
 
 PLAYER_SIZE = (100, 100)
 PLAYER_IMAGE = pygame.transform.scale(
-    pygame.image.load(os.path.join("Assets", "player.png")), PLAYER_SIZE
+    pygame.image.load("C:/Users/User/Documents/GitHub/i hate thiss/max123/Assets/player.png"), PLAYER_SIZE
 )
+PLAYER_IMAGE = pygame.transform.scale(
+    pygame.image.load(""), PLAYER_SIZE
--- Source/sprites.py
+++ Source/sprites.py
@@ -6,7 +6,7 @@ from Source.constants import SCREEN_SIZE
 
 
 class Sprite:
-    def __init__(self, start_position: tuple[int, int], size: tuple[int, int], image: pygame.Surface):
+    def __init__(self, start_position, size, image: pygame.Surface):
         self.__rect__ = pygame.rect.Rect(*start_position, *size)
         self.__start_position__ = start_position
         self.__size__ = size
@@ -14,7 +14,7 @@ class Sprite:
         self.__image__ = image
         self.__angle__ = 0
 
-    def get_coordinates(self) -> tuple[int, int]:
+    def get_coordinates(self):
         return self.__rect__.x, self.__rect__.y
 
     def change_coordinates(self, x: int = 0, y: int = 0) -> None:
