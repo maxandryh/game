692561767377aba3d0de5d273bff33281cc41842
deadinside666ua <129310872+AndrianaOz@users.noreply.github.com>
2023-05-27 18:30:13 +0300

    d
    d

--- Source/constants.py
+++ Source/constants.py
@@ -9,5 +9,5 @@ PLAYER_SIZE = (100, 100)
 PLAYER_IMAGE = pygame.transform.scale(
     pygame.image.load("C:/Users/User/Documents/GitHub/i hate thiss/max123/Assets/player.png"), PLAYER_SIZE
 )
-PLAYER_IMAGE = pygame.transform.scale(
-    pygame.image.load(""), PLAYER_SIZE
+BACKGROUND_IMAGE = pygame.transform.scale(
+    pygame.image.load("C:/Users/User/Documents/GitHub/i hate thiss/max123/Assets/backgraund.png"), PLAYER_SIZE
